75ff11beb3fe5f170afe4504850540b70ebf6c84
alamasyarie <maiaelarsya@gmail.com>
2021-06-21 00:51:53 +0700

    Footer section

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <footer>Copyright 2021 Alam Asy'arie</footer>;
+  return (
+    <footer className="bg-gray-300 py-1 font-light flex justify-center text-gray-500">
+      Copyright 2021 Alam Asy'arie
+    </footer>
+  );
 };
 
 export default Footer;
